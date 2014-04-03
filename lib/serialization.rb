985dd845fef4da90ee9151ce991a6d7e62bc1a35
jam231 <mateusz.jany@gmail.com>
2014-04-03 23:35:49 +0200

    Refactoring of utf8 serialization unit tests.

---
+++ lib/serialization.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+# Serialization
+module Serializer
+	@supported_types = [:uint8, :uint16, :uint32, :utf8]
+	def serialize(values, types)
+
+	end
+
+	def uint8
+	end
+
+	def uint16
+	end
+
+	def uint32
+	end
+
+	def utf8
+	end
+end
+
+
--- test/test_serialization.rb
+++ test/test_serialization.rb
@@ -7,64 +7,33 @@ include Serializer
 
 class TestSerializationUtf8 < Test::Unit::TestCase
 	def test_empty_string
-		assert_equal "", Serializer::serialize("", :utf8)	
+		assert_equal "", Serializer::serialize("", :utf8).first	
 	end
 
 	def test_ascii_string
-		assert_equal "sample text".bytes.to_a.pack('U*'), Serializer::serialize("sample text", :utf8)
+		assert_equal "sample text".bytes.to_a.pack('U*'), Serializer::serialize("sample text", :utf8).first
 	end
 
 	def test_utf8_string
-		assert_equal "tąśćt".bytes.to_a.pack('U*'), Serializer::serialize("tąśćt", :utf8)
+		assert_equal "tąśćt".bytes.to_a.pack('U*'), Serializer::serialize("tąśćt", :utf8).first
 	end
 
 	def test_typecheck_integer
-		assert_raise(RuntimeError) { Serializer::serialize(4, :utf8) }
+		assert_raise(ArgumentError) { Serializer::serialize(4, :utf8) }
 	end
 
 	def test_typecheck_array1
-		assert_raise(RuntimeError) { Serializer::serialize([], :utf8) }
+		assert_raise(ArgumentError) { Serializer::serialize([1], :utf8) }
 	end
 
 	def test_typecheck_array2
-		assert_raise(RuntimeError) { Serializer::serialize(["test string"], :utf8) }
+		assert_raise(ArgumentError) { Serializer::serialize(["test string", "54535"], :utf8) }
 	end
 
 	def test_typecheck_array3
-		assert_raise(RuntimeError) { Serializer::serialize(["test string", 4], :utf8) }
+		assert_raise(ArgumentError) { Serializer::serialize(["test string", 4], :utf8) }
 	end
 end
 
-
-class TestSerializetionUint8 < Test::Unit::TestCase
-	def test_zero
-		assert_equal [1].pack('C'), Serializer::serialize(0, :uint8)	
-	end
-
-	def test_one
-		assert_equal [1].pack('C'), Serializer::serialize(1, :uint8)
-	end
-
-	def test_max_value
-		value = 1 << 8 - 1
-		assert_equal [value].pack('C'), Serializer::serialize(value, :uint8)
-	end
-
-	def test_typecheck_integer
-		assert_raise(RuntimeError) { Serializer::serialize(4, :utf8) }
-	end
-
-	def test_typecheck_array1
-		assert_raise(RuntimeError) { Serializer::serialize([], :utf8) }
-	end
-
-	def test_typecheck_array2
-		assert_raise(RuntimeError) { Serializer::serialize(["test string"], :utf8) }
-	end
-
-	def test_typecheck_array3
-		assert_raise(RuntimeError) { Serializer::serialize(["test string", 4], :utf8) }
-	end
-end
 
 
